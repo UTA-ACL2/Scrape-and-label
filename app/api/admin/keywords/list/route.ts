bb6a3ccb64e67ce72b595f686bd94fe26fd84a0e
Lekhak123 <hridayesh.lekhak@uta.edu>
2024-06-03 00:41:58 -0500

    debug error

--- app/api/admin/keywords/list/route.ts
+++ app/api/admin/keywords/list/route.ts
@@ -1,17 +1,26 @@
 export const dynamic = "force-dynamic";
-import { NextRequest, NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import keyword from '@/models/keywordModel';
 import getDatabase from "@/database/database";
 import keywordGroupModel from '@/models/keywordGroupModel';
 
-export async function GET(request: NextRequest) {
-    await getDatabase();
-    const allItems = await keyword.find({}).populate('superset',keywordGroupModel);
-    const formattedItems = allItems.map((item, index) => ({
-        "id": item._id,
-        "name": item.keyword,
-        "superset": item.superset ? item.superset.keyword : "N/A",
-        "supersetid": item.superset
-    }));
-    return NextResponse.json(formattedItems);
+export async function GET(request : NextRequest) {
+    try {
+
+        await getDatabase();
+        const allItems = await keyword
+            .find({})
+            .populate('superset', keywordGroupModel);
+        const formattedItems = allItems.map((item, index) => ({
+            "id": item._id,
+            "name": item.keyword,
+            "superset": item.superset
+                ? item.superset.keyword
+                : "N/A ",
+            "supersetid": item.superset
+        }));
+        return NextResponse.json(formattedItems);
+    } catch (error) {
+        return NextResponse.json({error: error});
+    }
 }
