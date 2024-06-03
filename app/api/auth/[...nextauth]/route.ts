c9546ef1ec9c6a8bd8da646512ca791f794fe86b
Lekhak123 <hridayesh.lekhak@uta.edu>
2024-06-02 21:45:34 -0500

    rever next auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
-import {NextApiRequest, NextApiResponse} from "next";
-import {authOptions} from "./options";
 
-export default function auth(req : NextApiRequest, res : NextApiResponse) {
-    req.headers["x-forwarded-host"] = process.env.NEXTAUTH_URL;
-    return NextAuth(req, res, authOptions);
-}
+import { authOptions } from "./options";
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
