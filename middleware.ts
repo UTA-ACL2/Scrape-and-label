bfa09760590bdf5e7d6f00b35ca92f1d2f41bf15
Lekhak123 <hridayesh.lekhak@uta.edu>
2024-06-02 22:35:34 -0500

    fix middleware for prod

--- middleware.ts
+++ middleware.ts
@@ -13,19 +13,10 @@ async function middleware(req) {
     const isAuthenticated = !!(req
         ?.nextauth
             ?.token);
-    const isPublicRoute = req
-        .nextUrl
-        .pathname
-        .startsWith('/login');
-    const isAdminRoute = req
-        .nextUrl
-        .pathname
-        .startsWith('/api/admin') ||req
-        .nextUrl
-        .pathname
-        .startsWith('/admin') ;
+    const isPublicRoute = req.nextUrl.pathname.startsWith('/webapps/anivoice/login');
+    const isAdminRoute = req.nextUrl.pathname.startsWith('/webapps/anivoice/api/admin') || req.nextUrl.pathname.startsWith('/webapps/anivoice/admin');
     if (!isAuthenticated && !isPublicRoute) {
-        url.pathname = '/login';
+        url.pathname = '/webapps/anivoice/login';
         return NextResponse.rewrite(url)
     } else if (isAdminRoute && !(['usurper', 'admin'].includes(req?.nextauth?.token?.role as string))) {
         return NextResponse.json({
@@ -54,5 +45,5 @@ export const config = {
        * - favicon.ico (favicon file)
        * - login (login route)
        */
-        '/((?!_next/static|_next/image|favicon.ico|login).*)']
+        '/webapps/anivoice/((?!_next/static|_next/image|favicon.ico|login).*)']
 }
