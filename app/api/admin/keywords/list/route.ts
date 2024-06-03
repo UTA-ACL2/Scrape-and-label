91fecab5500a2bf519060cdd7bd4ddbc1e8a4f72
Lekhak123 <hridayesh.lekhak@uta.edu>
2024-06-03 00:54:41 -0500

    model find issue

--- app/api/admin/keywords/list/route.ts
+++ app/api/admin/keywords/list/route.ts
@@ -8,19 +8,25 @@ export async function GET(request : NextRequest) {
     try {
 
         await getDatabase();
-        const allItems = await keyword
+        keyword
             .find({})
             .populate('superset', keywordGroupModel)
-            .exec();
-        const formattedItems = allItems.map((item, index) => ({
-            "id": item._id,
-            "name": item.keyword,
-            "superset": item.superset
-                ? item.superset.keyword
-                : "N/A ",
-            "supersetid": item.superset
-        }));
-        return NextResponse.json(formattedItems);
+            .exec()
+            .then(allItems => {
+                const formattedItems = allItems.map((item, index) => ({
+                    "id": item._id,
+                    "name": item.keyword,
+                    "superset": item.superset
+                        ? item.superset.keyword
+                        : "N/A ",
+                    "supersetid": item.superset
+                }));
+                return NextResponse.json(formattedItems);
+            })
+            .catch((error:any) => {
+                return NextResponse.json({error: error.message});
+            });
+            return NextResponse.json([]);
     } catch (error:any) {
         return NextResponse.json({error: error.message});
     }
