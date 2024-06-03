57d35167908e321346f90f0406649d06b26ef4f6
Lekhak123 <hridayesh.lekhak@uta.edu>
2024-06-03 01:07:24 -0500

    rever

--- app/api/admin/keywords/list/route.ts
+++ app/api/admin/keywords/list/route.ts
@@ -1,32 +1,31 @@
-export const dynamic = "force-dynamic";
 import {NextRequest, NextResponse} from 'next/server';
 import keyword from '@/models/keywordModel';
-import getDatabase from "@/database/database";
+import getDatabase from '@/database/database';
 import keywordGroupModel from '@/models/keywordGroupModel';
+
+export const dynamic = 'force-dynamic';
 
 export async function GET(request : NextRequest) {
     try {
-
         await getDatabase();
-        keyword
+
+        const allItems = await keyword
             .find({})
-            .populate('superset', keywordGroupModel)
-            .exec()
-            .then(allItems => {
-                const formattedItems = allItems.map((item, index) => ({
-                    "id": item._id,
-                    "name": item.keyword,
-                    "superset": item.superset
-                        ? item.superset.keyword
-                        : "N/A ",
-                    "supersetid": item.superset
-                }));
-                return NextResponse.json(formattedItems);
-            })
-            .catch((error:any) => {
-                return NextResponse.json({error: error.message});
-            });
-    } catch (error:any) {
+            .populate('superset', keywordGroupModel);
+
+        const formattedItems = allItems.map((item) => ({
+            id: item._id,
+            name: item.keyword,
+            superset: item.superset
+                ? item.superset.keyword
+                : 'N/A',
+            supersetid: item.superset
+                ? item.superset._id
+                : 'N/A'
+        }));
+
+        return NextResponse.json(formattedItems);
+    } catch (error : any) {
         return NextResponse.json({error: error.message});
     }
 }
